0bd6f4d0fd6e225eedadfe3c6a2413439fb5312b
basecamp <jspence@basecampcodingacademy.org>
2017-03-21 13:14:44 -0500

    added the actual tmeplated and routes to them

--- rent/rental/views.py
+++ rent/rental/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
+def home(request):
+    return render(request, "rental/index.html")
+
+
 def store(request):
-    return HttpResponse("this is the store")
+    return render(request, "rental/store.html")
