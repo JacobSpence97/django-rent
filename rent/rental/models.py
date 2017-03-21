ada1036ad93dbf3a286559ade2d7411afd642ca5
basecamp <jspence@basecampcodingacademy.org>
2017-03-21 15:21:05 -0500

    added rental model

--- rent/rental/models.py
+++ rent/rental/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class rental(models.Model):
+    name = models.CharField(max_length=400)
+    img = models.CharField(max_length=400)
+    rental_type = models.CharField(max_length=400)
+    rent_val = models.CharField(max_length=400)
+    rep_val = models.CharField(max_length=400)
