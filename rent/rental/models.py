a2d1c154e8bf0f72515bcfd9d153a5aa7e5047c1
basecamp <jspence@basecampcodingacademy.org>
2017-03-22 09:33:07 -0500

    added variables for the template

--- rent/rental/models.py
+++ rent/rental/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 # Create your models here.
 class Rental(models.Model):
-    name = models.CharField(max_length=400)
     img = models.CharField(max_length=400)
+    name = models.CharField(max_length=400)
     rental_type = models.CharField(max_length=400)
     rent_val = models.CharField(max_length=400)
     rep_val = models.CharField(max_length=400)
