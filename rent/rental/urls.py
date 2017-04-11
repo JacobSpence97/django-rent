5dfcbd7ef1321534f39149162104a67cfe6001c4
basecamp <jspence@basecampcodingacademy.org>
2017-04-11 08:45:42 -0500

    fixed nav bar links

--- rent/rental/urls.py
+++ rent/rental/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.home),
+    url(r'^$', views.home, name='home'),
     url(r'^store', views.store, name="store"),
     url(r'^rent/(?P<item_id>[0-9]+)/$', views.rent, name="rent"),
     url(r'^returns/(?P<item_id>[0-9]+)/$', views.returns, name="returns"),
